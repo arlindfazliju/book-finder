b2b32ea8602eced9ddca98e952c5f5b27dba8184
arlindfazliju <arlindfazliu96@gmail.com>
2020-06-19 17:35:10 +0200

    Updates

---
+++ precache-manifest.73f0feacdce5b222ab76ed5191996297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3a99b58baf5a2069eebe346afa9d15",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "b530171a18d61711aa35",
+    "url": "/moviefinder/static/css/main.1f49ebd8.chunk.css"
+  },
+  {
+    "revision": "28100d922fa43f719c32",
+    "url": "/moviefinder/static/js/2.7267aa57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moviefinder/static/js/2.7267aa57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b530171a18d61711aa35",
+    "url": "/moviefinder/static/js/main.bde7ce9b.chunk.js"
+  },
+  {
+    "revision": "666612c9e8fb8035257b",
+    "url": "/moviefinder/static/js/runtime-main.d18c6a6d.js"
+  }
+]);
